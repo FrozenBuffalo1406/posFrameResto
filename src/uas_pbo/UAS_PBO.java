18f4b7b1ee084e847c9f2d7479b5b9381cc9a4f3
FrozenBuffalo1406 <ahmadfauzirizky003@gmail.com>
2023-05-30 19:15:24 +0100

    Initial commit

---
+++ src/uas_pbo/UAS_PBO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uas_pbo;
+
+/**
+ *
+ * @author User
+ */
+public class UAS_PBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mainFrame home = new mainFrame();
+        home.setTitle(" Acumalaka Eatery and Baverage");
+    }
+    
+}
